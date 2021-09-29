dbcd8900e0522889891882750881b849c991a7e4
LucasPr014 <91582073+LucasPr014@users.noreply.github.com>
2021-09-29 15:15:24 -0300

    Update teste_1.cpp

--- teste_1.cpp
+++ teste_1.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-       auto soma(auto a, auto b){
+auto soma(auto a, auto b){
        return a + b;
        }
-       int main(){
-       std::cout << soma(5, 6) << '\n';
-       return 0;
-       }
+int main(){
+std::cout << soma(5, 6) << '\n';
+return 0;
+ }
